e2a7347807eb0e27759da80269ea2e9f7675b2ad
Jeroen de Baat <jeroendebaat@gmail.com>
2020-11-18 13:56:38 +0100

    Mostly refactors

--- hades.py
+++ hades.py
@@ -1,52 +1,50 @@
 from difflib import SequenceMatcher
+import math
 import itertools
 import os
 import pathlib
 import pprint
 
 def main():
+    DIRNAME = './';
     NUMBER_OF_TOP_RESULTS = 20
     REPORTS_DIR = 'reports'
-    dirName = './';
 
-    # Get the list of all files in directory tree at given path
-    listOfFiles = list()
-    for (dirpath, _, filenames) in os.walk(dirName):
-        listOfFiles += [os.path.join(dirpath, file) for file in filenames if file.endswith('.c')]
+    runPlagiarismCheck(DIRNAME, NUMBER_OF_TOP_RESULTS, REPORTS_DIR)
 
-    # Read all files into a list of strings
-    fileStrings = list()
-    for filePath in listOfFiles:
-        with open(filePath, 'r') as file:
-            fileStrings.append(file.read().replace('\n', ''))
 
-    print("Comparing", len(listOfFiles), "files...", end='', flush=True)
+def runPlagiarismCheck(dirName, numberOfTopResults, reportsDir):
+    filePaths = getListOfFiles(dirName)
+    strings = filePathsToStrings(filePaths)
+
+    print("Comparing", len(strings), "files in",
+          math.comb(len(strings), 2), "combinations...", end='', flush=True)
 
     # Create a list of tuples (ratio, filePath0, filePath1)
-    ratios = list()
-    for i, j in itertools.combinations(range(len(fileStrings)), 2):
-#        ratio = SequenceMatcher(None, fileStrings[i], fileStrings[j]).ratio()
-        ratio = SequenceMatcher(lambda c: c.isspace(), fileStrings[i], fileStrings[j]).ratio()
-        tup = ratio, listOfFiles[i], listOfFiles[j]
+    ratios = []
+    for i, j in itertools.combinations(range(len(strings)), 2):
+#        ratio = SequenceMatcher(None, strings[i], strings[j]).ratio()
+        ratio = SequenceMatcher(lambda c: c.isspace(), strings[i], strings[j]).ratio()
+        tup = ratio, filePaths[i], filePaths[j]
         ratios.append(tup)
     print(' Done')
 
     # Sort to find the files which are most similar
     print('Sorting ratios...', end='', flush=True)
     ratios.sort(reverse=True)
-    bestMatches = ratios[0:NUMBER_OF_TOP_RESULTS]
+    bestMatches = ratios[0:numberOfTopResults]
     print(' Done')
 
-    print('Showing the ' + str(NUMBER_OF_TOP_RESULTS) + ' best matches:')
+    print('Showing the ' + str(numberOfTopResults) + ' best matches:')
     pp = pprint.PrettyPrinter(width=200)
     pp.pprint(bestMatches)
 
-    # Generate report
+    # Generate reports
     print('Generating reports...', end='', flush=True)
     separator = '\n' + '-' * 80 + '\n'
-    pathlib.Path(REPORTS_DIR).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(reportsDir).mkdir(parents=True, exist_ok=True)
     for idx, (ratio, filePath0, filePath1) in enumerate(bestMatches):
-        with open(REPORTS_DIR + '/' + str(idx) + '.txt', 'w') as file:
+        with open(reportsDir + '/' + str(idx) + '.txt', 'w') as file:
             file.write('Match ratio: ' + str(ratio) + '\n')
             file.write('File 0: ' + filePath0 + '\n')
             file.write('File 1: ' + filePath1 + '\n')
@@ -56,7 +54,24 @@ def main():
             file.write(separator)
             with open(filePath1, 'r') as file1:
                 file.write(file1.read())
-    print(' Reports written to ' + REPORTS_DIR + '/')
+    print(' Reports written to ' + reportsDir + '/')
+
+
+def getListOfFiles(dirName):
+    '''Get the list of all files in directory tree at given path'''
+    filePaths = []
+    for (dirpath, _, filenames) in os.walk(dirName):
+        filePaths += [os.path.join(dirpath, file) for file in filenames if file.endswith('.py')]
+    return filePaths
+
+
+def filePathsToStrings(filePaths):
+    '''Reads the files indicates by filePaths into a list of strings'''
+    strings = []
+    for filePath in filePaths:
+        with open(filePath, 'r') as file:
+            strings.append(file.read().replace('\n', ''))
+    return strings
 
 
 if __name__ == '__main__':
